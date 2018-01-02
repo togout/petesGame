3353702901bbedd4740b4d1263842ca34e5bd45a
togout <34657483+togout@users.noreply.github.com>
2018-01-02 19:10:56 +0000

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 var sprites = {
- ship: { sx: 0, sy: 0, w: 37, h: 42, frames: 1 },
+ ship: { sx: 0, sy: 0, w: 17, h: 42, frames: 1 },
  missile: { sx: 0, sy: 30, w: 2, h: 10, frames: 1 },
  enemy_purple: { sx: 37, sy: 0, w: 42, h: 43, frames: 1 },
  enemy_bee: { sx: 79, sy: 0, w: 37, h: 43, frames: 1 },
